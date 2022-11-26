e5a84bee3ed98a0c9aeb6ee33f6b84d22bbffb73
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-11-26 13:56:46 +0200

    Designed Register page

--- Client/src/app/register/register.page.ts
+++ Client/src/app/register/register.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { USER_RESPONSE } from '../models/users-model';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +9,16 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage implements OnInit {
 
+  confirm_password?: string = undefined;
+  new_user: USER_RESPONSE = {
+
+    username: undefined,
+    email: undefined,
+    gender: undefined,
+    birthday: new Date(),
+    password: undefined
+
+  };
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -16,6 +27,13 @@ export class RegisterPage implements OnInit {
   public goToLogin() {
 
     this.router.navigate(['/login'])
+
+  }
+
+  public onRegister() {
+
+    console.log(this.new_user.birthday);
+
 
   }
 
