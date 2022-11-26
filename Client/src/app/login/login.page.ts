5905599753446da07f958ef8c262cd63e562aa3c
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-11-26 10:51:07 +0200

    Designed basic login

--- Client/src/app/login/login.page.ts
+++ Client/src/app/login/login.page.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  email: string = "";
+  password: string = "";
   constructor() { }
 
   ngOnInit() {
