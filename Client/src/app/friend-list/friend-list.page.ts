c50b71ec961382b721400d8561e558f0ba0d6dc6
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-11-26 20:30:04 +0200

    Designed the list

--- Client/src/app/friend-list/friend-list.page.ts
+++ Client/src/app/friend-list/friend-list.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FollowService } from '../apis/follow.service';
 import { StorageService } from '../apis/storage.service';
 import { UserService } from '../apis/user.service';
 import { User } from '../models/users-model';
@@ -12,29 +13,54 @@ import { User } from '../models/users-model';
 export class FriendListPage implements OnInit {
 
   current_user?: User;
-  followers?: Map<number, User>;
-  followings?: Map<number, User>;
-  constructor(route: ActivatedRoute, storage: StorageService, userService: UserService) {
+  followers: Set<User> = new Set();
+  followings: Set<User> = new Set();
+  section: string = "default";
+  
+  constructor(private route: ActivatedRoute, private storage: StorageService, private userService: UserService, private followService: FollowService) {
 
-    // const id_obj = route.snapshot.paramMap.get("id");
-    // const id: number = Number.parseInt(id_obj != null ? id_obj : "0");
-    // if (id == null || !id) {
+    const id_obj = route.snapshot.paramMap.get("id");
+    const id: number = Number.parseInt(id_obj != null ? id_obj : "0");
+    if (id == null || !id) {
 
-    //   storage.get("loggedInUser").then((r) => {
+      this.storage.get("loggedInUser").then((r) => {
 
-    //     this.current_user = <User>r;
+        this.current_user = <User>r;
+        this.generateUsers();
 
-    //   });
+      });
 
-    // } else {
+    } else {
 
-    //     userService.getOne({ user_id: id }).subscribe(r => {
+      this.userService.getOne({ user_id: id }).subscribe(r => {
 
-    //     this.current_user = r.user;
+        this.current_user = r.user;
+        this.generateUsers();
 
-    //   })
+      })
+    }
 
-    // }
+  }
+
+  generateUsers() {
+
+    console.log("FOLL");
+    this.followService.getFollowings(this.current_user!.user_id).subscribe(response =>
+
+      response.users?.forEach(current =>
+        this.followings.add(current))
+
+    );
+    this.followService.getFollowers(this.current_user!.user_id).subscribe(response =>
+
+
+      response.users?.forEach(current =>
+        this.followers.add(current)
+      )
+
+    );
+    console.log(this.followings);
+    console.log(this.followers);
 
   }
 
