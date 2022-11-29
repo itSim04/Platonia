80b0d96b95f1cf67ea80095fbfae03830c6a75da
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-11-29 13:08:48 +0200

    Implemented liking

--- Client/src/app/components/thought/thought.component.ts
+++ Client/src/app/components/thought/thought.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { LikeService } from 'src/app/apis/like.service';
 import { StorageService } from 'src/app/apis/storage.service';
+import { ThoughtService } from 'src/app/apis/thought.service';
+import { EXIT_CODES } from 'src/app/constants';
 import { Thought } from 'src/app/models/thoughts-model';
 import { User } from 'src/app/models/users-model';
 
@@ -8,17 +11,47 @@ import { User } from 'src/app/models/users-model';
   templateUrl: './thought.component.html',
   styleUrls: ['./thought.component.scss'],
 })
-export class ThoughtComponent {
+export class ThoughtComponent implements OnInit {
 
   session_user?: User;
   @Input() user?: User;
   @Input() thought!: Thought;
   @Input() editable?: boolean;
-  constructor(private storageService: StorageService) {
+  constructor(private storageService: StorageService, private likeService: LikeService) {
   }
 
-  click() {
-    console.log(this.user?.username, this.user);
+  ngOnInit(): void {
+    this.storageService.get<User>("loggedInUser").then(r => this.session_user = r);
+  }
+
+  like() {
+
+    if (this.thought.is_liked) {
+      this.thought.is_liked = false;
+      this.thought.likes--;
+      this.likeService.unlike(this.session_user!.user_id, this.thought.thought_id).subscribe(r => {
+        
+        console.log(r);
+        if (r.status != EXIT_CODES.LIKE_REMOVE) {
+          this.thought.is_liked = true;
+          this.thought.likes++;
+        }
+
+      });
+
+
+    } else {
+      this.thought.is_liked = true;
+      this.thought.likes++;
+      this.likeService.like(this.session_user!.user_id, this.thought.thought_id).subscribe(r => {
+
+        if (r.status != EXIT_CODES.LIKE_ADD) {
+          this.thought.is_liked = false;
+          this.thought.likes--;
+        }
+
+      })
+    }
   }
 
 }
