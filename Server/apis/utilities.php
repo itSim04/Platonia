315cf9d3f4c3dcd05c7c57da5b9a9ebd09e67586
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-12-09 12:20:04 +0200

    Prepared backend mailing

--- Server/apis/utilities.php
+++ Server/apis/utilities.php
@@ -2,21 +2,35 @@
 
 require '../helper/root.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../helper/PHPMailer/Exception.php';
+require '../helper/PHPMailer/PHPMailer.php';
+require '../helper/PHPMailer/SMTP.php';
+
 if (check_keys($_GET, "schema")) {
 
 
     switch ($_GET["schema"]) {
 
         case UTILITIES::EMAIL:
+            try {
+                $mail = new PHPMailer(true);
 
-            //if(check_keys(EMAIL::MESSAGE)) {
+                $mail->Port = 465;
 
-            $msg = "First line of text\nSecond line of text";
+                $mail->SetFrom("mo@domain.co","my name", 0);
+                $mail->addAddress("moawadsimon@gmail.com");
 
-            // use wordwrap() if lines are longer than 70 characters
-            $msg = wordwrap($msg, 70);
+                $mail->isHTML(true);
+                $mail->Subject = 'Here is the subject';
+                $mail->Body = 'This is the HTML message body <b>in bold!</b>';
 
-            // send email
-
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
         //}
     }}
