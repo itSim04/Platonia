ac3a5e83baf23010744018ea9dc5ca211d2b5f85
Simon Mouawad <43381860+itSim04@users.noreply.github.com>
2022-12-13 17:19:27 +0200

    Handled new chats

--- Client/src/app/pages/browsing/messaging/messaging.page.ts
+++ Client/src/app/pages/browsing/messaging/messaging.page.ts
@@ -1,7 +1,7 @@
 import { UserService } from './../../../linking/apis/user.service';
 import { StorageService } from './../../../linking/apis/storage.service';
 import { Component, OnInit } from '@angular/core';
-import { Database, get, getDatabase, onValue, ref, set, DatabaseReference, child, update } from '@angular/fire/database';
+import { Database, get, getDatabase, onValue, ref, set, DatabaseReference, child, update, runTransaction } from '@angular/fire/database';
 import { User } from 'src/app/linking/models/user-main';
 
 @Component({
@@ -14,6 +14,8 @@ export class MessagingPage implements OnInit {
   complete_users: Array<User> = new Array();
   users: Array<User> = new Array();
 
+  chats: Array<string> = new Array();
+
   isNewOpen: boolean = false;
 
 
@@ -25,21 +27,26 @@ export class MessagingPage implements OnInit {
   }
 
   ngOnInit() {
+
     this.storageService.getSessionUser().then(r => {
 
-
       this.session_user = r
+      const chatRef = ref(this.db, 'users/' + r.user_id + '/chats/');
+      onValue(chatRef, (snapshot) => {
+        const data = snapshot.val();
+        this.chats.splice(0);
+        if (data) {
+          Object.keys(data).forEach((element: any) => {
+            this.chats.push(element);
+          });
+        }
+
+
+      });
 
 
     });
 
-    this.userService.getAll().subscribe(r => r.users?.forEach(u => {
-
-      //this.complete_users.splice(0);
-      this.complete_users.push(u);
-      this.users.push(u);
-
-    }));
 
 
 
@@ -53,17 +60,70 @@ export class MessagingPage implements OnInit {
   }
 
   addChat(user: User) {
-    
-    update(ref(this.db, "users/" + this.session_user.user_id + "/chats/"), {
 
-      [Math.min(this.session_user.user_id, user.user_id) + "-" + Math.max(this.session_user.user_id, user.user_id)]: true
+    if (user.user_id != this.session_user.user_id) {
+
+      const id: string = Math.min(this.session_user.user_id, user.user_id) + "-" + Math.max(this.session_user.user_id, user.user_id);
+      const db: Database = getDatabase();
+      const postRef: DatabaseReference = ref(db, "users/" + this.session_user.user_id + "/chats/");
+
+      runTransaction(postRef, (post) => {
+
+        if (!post) {
+
+          post = {
+
+            [id]: new Date().toISOString()
+
+          }
+
+        } else if (!post[id]) {
+
+          post[id] = new Date().toISOString()
+
+        }
+        return post;
+      });
+
+      const chatRef: DatabaseReference = ref(db, "chats/" + id);
+
+      runTransaction(chatRef, (post) => {
+
+        if (!post) {
+
+          post = {
+
+            title: id,
+            start: new Date().toISOString()
+
+
+          }
+
+        }
+
+        return post;
 
       });
+
+    }
+
   }
 
   toggleNew(state: boolean) {
 
-    this.isNewOpen = state;
+    this.complete_users.splice(0);
+    this.users.splice(0);
+    this.userService.getAll().subscribe(r => r.users?.forEach(u => {
+
+      if (u.user_id != this.session_user.user_id) {
+        //this.complete_users.splice(0);
+        this.isNewOpen = state;
+        this.complete_users.push(u);
+        this.users.push(u);
+      }
+      
+    }));
+
 
   }
 
